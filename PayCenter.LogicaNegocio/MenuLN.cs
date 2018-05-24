3388405390cf1fd3fd64da37ade429bfea57c75e
AmadoDomper <amado.domper@gmail.com>
2018-05-24 00:03:20 -0500

    Create Menu Options

---
+++ PayCenter.LogicaNegocio/MenuLN.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PayCenter.EntidadesNegocio;
+using PayCenter.AccesoDatos;
+
+namespace PayCenter.LogicaNegocio
+{
+    public class MenuLN
+    {
+        MenuAD oMenuAD;
+
+        public MenuLN()
+        {
+            oMenuAD = new MenuAD();
+        }
+
+        public List<Menu> ObtenerMenusFull(int nRolId)
+        {
+            try
+            {
+                return oMenuAD.ObtenerMenuFull(nRolId);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }    }
+}
