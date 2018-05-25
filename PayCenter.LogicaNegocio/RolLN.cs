c95a9ea7e3af34eb6ff6bf53d2c9092cbdd23c9d
AmadoDomper <amado.domper@gmail.com>
2018-05-25 02:34:04 -0500

    Create Config Modulo

---
+++ PayCenter.LogicaNegocio/RolLN.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PayCenter.EntidadesNegocio;
+using PayCenter.AccesoDatos;
+
+namespace PayCenter.LogicaNegocio
+{
+    public class RolLN
+    {
+        RolAD oRolAD;
+
+        public RolLN()
+        {
+            oRolAD = new RolAD();
+        }
+
+        public List<Rol> ListarRoles()
+        {
+            return oRolAD.ListarRoles();
+        }
+    }
+}
---
+++ PayCenter.Web/Helper/Constantes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PayCenter.Web.Helper
+{
+    public class Constantes
+    {
+        public const int Rol_Administrador = 1;
+    }
+}
